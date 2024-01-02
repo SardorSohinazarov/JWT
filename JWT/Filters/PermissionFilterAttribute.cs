54111d85acf719ef33fb04bfbdb0821d810403f0
SardorSohinazarov <sardorstudent0618gmail.com>
2024-01-02 23:37:11 +0500

    New Auth Filter Implemeted

---
+++ JWT/Filters/PermissionFilterAttribute.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace JWT.Filters
+{
+    public class PermissionFilterAttribute : Attribute, IAuthorizationFilter
+    {
+        private readonly string _permission;
+
+        public PermissionFilterAttribute(string permission)
+            => _permission = permission;
+
+        public void OnAuthorization(AuthorizationFilterContext context)
+        {
+            if (!context.HttpContext.User.Identity.IsAuthenticated)
+            {
+                context.Result = new UnauthorizedResult();
+                return;
+            }
+
+            var permission = context.HttpContext.User.Claims.FirstOrDefault(x => x.Type == "permission" && x.Value == _permission);
+
+            if (permission == null)
+            {
+                context.Result = new ForbidResult();
+                return;
+            }
+        }
+    }
+}
